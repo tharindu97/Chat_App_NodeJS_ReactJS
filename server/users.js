ed995de484ad4b59098a303d9ec3d969f0b446e4
tharindu97 <tharindukavishna@gmail.com>
2020-06-04 23:07:02 +0530

    57_10

--- server/users.js
+++ server/users.js
@@ -1,8 +1,15 @@
 const users = [];
 
-const addUser = () => {
+const addUser = ({ id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
 
-}
+    const existingUser = users.find((user) => user.room === room && user.name === name);
+
+    if(existingUser){
+        return { error: 'Username is token'}
+        }
+    }
 const removeUser = () => {
 
 }
