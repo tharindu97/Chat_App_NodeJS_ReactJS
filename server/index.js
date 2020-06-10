a034bc5370d3c693c86f8194fd344d9458d3f550
tharindu97 <tharindukavishna@gmail.com>
2020-06-10 21:34:00 +0530

    01_08_57

--- server/index.js
+++ server/index.js
@@ -16,13 +16,23 @@ io.on('connection', (socket) => {
     console.log('We Have a New Connection.......');
 
     socket.on('join', ({ name, room }, callback) => {
-        console.log(name,room);
+       const { error, user } = addUser({ id: socket.id, name, room });
+       if(error) return callback(error);
 
-        const error = true;
+       socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+       socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
 
-        if(error){
-            callback({ error: 'error' });
-        }
+       socket.join(user.room);
+
+       callback();
+    });
+
+    socket.on('sendMessage', (message, callback) =>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text:message});
+
+        callback();
     });
 
     socket.on('disconnect', () => {
@@ -36,4 +46,4 @@ server.listen(PORT, () => {
     console.log(`Server has started on port ${PORT}`)
 });
 
-//50 18
+// 1 08 37
