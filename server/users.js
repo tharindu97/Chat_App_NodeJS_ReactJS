4a10025bac2faae116cc9b7bb59982894d00ba81
tharindu97 <tharindukavishna@gmail.com>
2020-06-10 20:39:47 +0530

    init

--- server/users.js
+++ server/users.js
@@ -8,19 +8,24 @@ const addUser = ({ id, name, room }) => {
 
     if(existingUser){
         return { error: 'Username is token'}
-        }
+    }
     
     const user = { id, name, room};
     users.push(user); 
 
     return { user }
 }
-const removeUser = (id) => {
-    
-}
-const getUser = () => {
 
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1){
+        return users.splice(index, 1)[0]
+    }
 }
-const getUsersInRoom = () => {
-    
-}
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room); 
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
