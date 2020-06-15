6cbd9250f597d9e536553fff662a91b53d7e5fc1
tharindu97 <tharindukavishna@gmail.com>
2020-06-15 21:17:56 +0530

    1_23_15

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,7 +8,8 @@ let socket;
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
-    const [messages, setMessages] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -19,7 +20,7 @@ let socket;
         setName(name);
         setRoom(room); 
 
-        socket.emit('join', { name, room }, ({ error }) =>{
+        socket.emit('join', { name, room }, () =>{
            
         });
 
@@ -31,12 +32,29 @@ let socket;
 
     useEffect(() =>{
         socket.on('message', (message) =>{
-
+            setMessages([...messages, message]);
         })
-    });
+    }, [messages]);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message){
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
 
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+            </div>
+        </div>
     )
  }
 
